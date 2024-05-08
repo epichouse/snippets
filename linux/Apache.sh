50e9ae870473c1e5b0ca3a1acd2eed6a754769b9
Dan <danshaw509@gmail.com>
2024-05-08 13:19:01 +0100

    Added Apache, NGINX, DNS and SSL examples

--- linux/Apache.sh
+++ linux/Apache.sh
@@ -1,1 +1,13 @@
-## Add apache commands here
+# Apache Commands
+
+# View all vhosts
+sudo apache2ctl -S
+
+# Config Test
+sudo apache2ctl configtest
+
+# Enable Site
+sudo a2ensite website.conf
+
+# Disable Site
+sudo a2dissite website.conf
---
+++ linux/Flush DNS.sh
@@ -0,0 +1,2 @@
+# On a system using systemd-resolved the command below can be used to flush DNS (Ubuntu)
+resolvectl flush-caches
