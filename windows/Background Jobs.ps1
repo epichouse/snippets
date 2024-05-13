e6baaec09621164a3bd8b2b0ab2b116da96e4b28
Dan <danshaw509@gmail.com>
2024-05-13 15:22:59 +0100

    Added remove-job

--- windows/Background Jobs.ps1
+++ windows/Background Jobs.ps1
@@ -4,5 +4,8 @@ Get-Job
 # Start Background Job
 Start-Job -ScriptBlock { Comand Here }
 
-# Stop Background Job
+# Stop Background Job (Job will be left in place in a stopped status)
 Stop-Job #Either use -name or -id from get-job command
+
+# Remove Background Job
+Remove-Job #Either use -name or -id from get-job command
