633853540ecab7ba076405ae7e4da3d13443511d
Dan <danshaw509@gmail.com>
2024-05-16 19:12:04 +0100

    Updated MacOS Dock Hide Commands

--- macos/Autohide Dock.sh
+++ macos/Autohide Dock.sh
@@ -1,2 +1,5 @@
-# The command below configures MacOS dock autohide to my liking
+# The command below configures MacOS dock autohide animation duration
 defaults write com.apple.dock autohide-time-modifier -float 0.75; killall Dock
+
+# The command below removes the delay in showing a hidden dock
+defaults write com.apple.dock autohide-delay -float 0; killall Dock
