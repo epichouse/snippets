572ff520506439270d43fee8ce54d14d6c455166
Dan <danshaw509@gmail.com>
2024-05-18 00:03:14 +0100

    Added additional commands to GIT

--- cross-platform/GIT.sh
+++ cross-platform/GIT.sh
@@ -1,6 +1,11 @@
 # Git Command Examples
 
-# Bare clones can be used to restore/migrate GIT repositories.
+# Clone a repository
+# The command below will clone the repository specified into a sub-dir of the current directory with the repository name.
+git clone git:{server}:{namespace/username}/{repository name}.git
+
+# Mark all non executable .sh files as executable in git repository
+git add . --chmod=+x
 
 # Create a local bare copy of a repository. (snippets in this case)
 # This will create a directory called snippets.git, containing the bare version of the repo rather than a working directory.
