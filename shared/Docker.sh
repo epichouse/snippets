dcc3e28c26a9258d1be140a9fc098d00862dae0e
Dan <danshaw509@gmail.com>
2024-05-21 10:23:46 +0100

    Docker Build Commands

--- shared/Docker.sh
+++ shared/Docker.sh
@@ -3,5 +3,14 @@
 # Run Container
 docker run -d --name webserver -d -p 8099:80 nginx
 
+# Login to Docker Hub
+docker login -u danshaw509
+
 # Build Container Image
-docker build . -t cloud46.ubuntu.v1
+docker build . -t epichouse.web:v1
+
+# Rename Image
+docker tag epichouse.web:v1 danshaw509/epichouse.web:v1
+
+# Push to Docker Hub
+docker push danshaw509/epichouse.web:v1
