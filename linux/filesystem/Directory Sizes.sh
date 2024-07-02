6f4f66b57784dc6b10717953e9a386eda8ffb718
Dan <danshaw509@gmail.com>
2024-07-02 21:15:17 +0100

    Added new filesystem sub dir in Linux

---
+++ linux/filesystem/Directory Sizes.sh
@@ -0,0 +1,11 @@
+# Full filesystem sizes
+sudo df -h
+
+# Get top level folder sizes
+du -cha --max-depth=1 /
+
+# All sub folders and files
+du -h /docker
+
+# One level deep
+du -ha /docker --max-depth=1
--- linux/filesystem/UNIX Permissions.sh
+++ linux/filesystem/UNIX Permissions.sh
@@ -1,15 +1,3 @@
-# Full filesystem sizes
-sudo df -h
-
-# Get top level folder sizes
-du -cha --max-depth=1 /
-
-# All sub folders and files
-du -h /docker
-
-# One level deep
-du -ha /docker --max-depth=1
-
 # Set owner/group of a folder as dan recursively
 sudo chown dan:dan -R /afolder
 
