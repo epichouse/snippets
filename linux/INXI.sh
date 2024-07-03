2faab9adb53440c6799015ea4639cfeb4e444fbf
Dan <danshaw509@gmail.com>
2024-07-03 17:59:22 +0100

    Added INXI

---
+++ linux/INXI.sh
@@ -0,0 +1,10 @@
+# INXI is a collection of handy command line tools that can be used to output information about the system in question.
+
+# Get Disks
+inxi -xD
+
+# CPU Information
+inxi -C
+
+# System Information
+inxi -b
