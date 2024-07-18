41eb8fd8b8e1d451e16f0488bd0a4df72db7b4f4
Dan <danshaw509@gmail.com>
2024-07-18 20:55:56 +0100

    Added Ansible Ad-Hoc tasks

--- linux/Ansible.sh
+++ linux/Ansible.sh
@@ -3,3 +3,14 @@ ansible-galaxy init {Role Name}
 
 # Run Playbook
 ansible-playbook ansible-playbook {Playbook Path}.yml --extra-vars "ansible_sudo_pass={Sudo Pass if Required}}" -i {Inventory File}.yml
+
+### Adhoc Commmands
+
+# List Hosts in inventory group global
+ansible global --list-hosts
+
+# execute ansible module ping on all hosts in group global
+ansible global -m ping
+
+# execute the command "echo test" on all hosts in group global
+ansible global -a "echo test"
