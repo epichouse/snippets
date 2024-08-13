dc51f681a45fccb0b448d7b42900f6b4f428531d
Dan <danshaw509@gmail.com>
2024-08-13 08:00:44 +0100

    update 'For Each Loops.sh'

--- linux/scripting/For Each Loops.sh
+++ linux/scripting/For Each Loops.sh
@@ -1,2 +1,4 @@
 # For directory in directory loop example
-for dir in /docker/*; do (cd "$dir" && docker compose down); done
+for dir in /docker/*; do (cd "$dir" && docker compose down && docker compose up -d); done
+
+for dir in /docker/apps/*; do (cd "$dir" && docker compose down && docker compose up -d); done
