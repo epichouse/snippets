974ac40cd698cf604397a3014f4866f90f017481
Dan <danshaw509@gmail.com>
2024-08-22 13:11:37 +0100

    update 'Cloudflared Client.ps1'

--- windows/Cloudflared Client.ps1
+++ windows/Cloudflared Client.ps1
@@ -7,3 +7,6 @@ cloudflared access rdp --hostname vdi.epichouse.co.uk --url rdp://localhost:3333
 
 # Open Tunnel as background job
 Start-Job -ScriptBlock { cloudflared access rdp --hostname vdi.epichouse.co.uk --url rdp://localhost:33333}
+
+# Create Tunnel Service (Change to run as me)
+New-Service -Name "CFTunnel" -BinaryPathName '"C:\Program Files (x86)\cloudflared\cloudflared.exe" access rdp --hostname dan-desktop.epichouse.co.uk --url localhost:33999'
